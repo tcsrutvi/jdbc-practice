d934415d8b25615d8ba788f1e348bf4de769a698
tcsrutvi <tcsrutvi@gmail.com>
2021-08-16 11:41:28 +0530

    first commit

---
+++ src/main/java/com/tcs/jdbc/demo/JDBCDemo.java
@@ -0,0 +1,39 @@
+package com.tcs.jdbc.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCDemo {
+
+	public static void main(String[] args) {
+		String DB_URL="jdbc:mysql://localhost/practice";
+		String DB_USER = "root";
+		String DB_PASSWORD="Nuvelabs123$";
+		
+		try(Connection connection=DriverManager.getConnection(DB_URL, DB_USER,DB_PASSWORD);
+				Statement statement = connection.createStatement();){
+			//statement.execute("INSERT INTO REGIONS VALUES(5, 'antarctica')");
+			retrieve(statement);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void retrieve(Statement statement) throws SQLException {
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM REGIONS");
+		while(resultSet.next()) {
+			System.out.println(resultSet.getInt(1));
+			System.out.println(resultSet.getNString("REGION_NAME"));
+		}
+	}
+	
+	private static void create(Statement statement) throws SQLException {
+		statement.execute("INSERT INTO REGIONS VALUES(8, 'North America')");
+	}
+	
+	
+}
